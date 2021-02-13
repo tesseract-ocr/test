d8a2e3223a65020b985211b50dc24dbb4ae67ef9
Shree Devi Kumar <5095331+Shreeshrii@users.noreply.github.com>
2021-02-13 09:40:52 +0000

    Modify as per review

--- testing/basicapitest.cpp
+++ testing/basicapitest.cpp
@@ -1,9 +1,9 @@
+#include <allheaders.h>
+#include <stdio.h>
 #include <tesseract/baseapi.h>
-#include <leptonica/allheaders.h>
 
 int main()
 {
-    char *outText;
 
     tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
     // Initialize tesseract-ocr with English
@@ -16,7 +16,7 @@ int main()
     Pix *image = pixRead("../test/testing/phototest.tif");
     api->SetImage(image);
     // Get OCR result
-    outText = api->GetUTF8Text();
+    char *outText = api->GetUTF8Text();
     printf("OCR output:\n%s", outText);
 
     // Destroy used object and release memory
